402fe34a1afab0cec0a67ac98426636e90f9ad04
Andrey Shklyaruk <13mailg@gmail.com>
2020-11-01 19:43:25 +0200

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,215 @@
-export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'nuxt-shop',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const isDev = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    mode: 'universal',
+    ...(!isDev && {
+        modern: 'client'
+    }),
+
+    head: {
+        htmlAttrs: {
+            lang: 'en'
+        },
+        title: 'Nuxt Shop',
+        meta: [
+            { hid: 'description', name: 'description', content: 'Online shop' }
+        ],
+        link: [
+            { rel: 'shortcut icon', href: 'favicon.ico' }
+        ]
+    },
+    rootDir: __dirname,
+    serverMiddleware: [
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+    router: {
+        prefetchLinks: false
+    },
+    loading: { color: '#ddd' },
+    css: [
+        'normalize.css',
+        './assets/scss/global-styles.scss'
+    ],
+    plugins: [
+    ],
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        'nuxt-trailingslash-module',
+        'nuxt-webfontloader',
+        'cookie-universal-nuxt',
+        '@nuxtjs/style-resources'
+    ],
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+    webfontloader: {
+        events: false,
+        google: {
+            families: ['Montserrat:400,500,600:cyrillic&display=swap']
+        },
+        timeout: 5000
+    },
+    styleResources: {
+        // your settings here
+        // scss: ['./assets/scss/global-variables.scss'], // alternative: scss
+        less: [],
+        stylus: []
+    },
+    /*
+    ** Axios module configuration
+    */
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+    },
+    render: {
+        // http2: {
+        //     push: true,
+        //     pushAssets: (req, res, publicPath, preloadFiles) => preloadFiles
+        //     .map(f => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`)
+        //   },
+        // compressor: false,
+        resourceHints: false,
+        etag: false,
+        static: {
+            etag: false
+        }
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+        optimizeCss: false,
+        filenames: {
+            app: ({ isDev }) => isDev ? '[name].js' : 'js/[contenthash].js',
+            chunk: ({ isDev }) => isDev ? '[name].js' : 'js/[contenthash].js',
+            css: ({ isDev }) => isDev ? '[name].css' : 'css/[contenthash].css',
+            img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[contenthash:7].[ext]',
+            font: ({ isDev }) => isDev ? '[path][name].[ext]' : 'fonts/[contenthash:7].[ext]',
+            video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[contenthash:7].[ext]'
+        },
+        ...(!isDev && {
+            html: {
+                minify: {
+                    collapseBooleanAttributes: true,
+                    decodeEntities: true,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    processConditionalComments: true,
+                    removeEmptyAttributes: true,
+                    removeRedundantAttributes: true,
+                    trimCustomFragments: true,
+                    useShortDoctype: true
+                }
+            }
+        }),
+        splitChunks: {
+            layouts: true,
+            pages: true,
+            commons: true
+        },
+        optimization: {
+            minimize: !isDev
+        },
+        ...(!isDev && {
+            extractCSS: {
+                ignoreOrder: true
+            }
+        }),
+        transpile: ['vue-lazy-hydration', 'intersection-observer'],
+        postcss: {
+            plugins: {
+                ...(!isDev && {
+                    cssnano: {
+                        preset: ['advanced', {
+                            autoprefixer: false,
+                            cssDeclarationSorter: false,
+                            zindex: false,
+                            discardComments: {
+                                removeAll: true
+                            }
+                        }]
+                    }
+                })
+            },
+            ...(!isDev && {
+                preset: {
+                    browsers: 'cover 99.5%',
+                    autoprefixer: true
+                }
+            }),
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+            order: 'cssnanoLast'
+        },
+        extend(config, ctx) {
+            const ORIGINAL_TEST = '/\\.(png|jpe?g|gif|svg|webp)$/i'
+            const vueSvgLoader = [
+                {
+                    loader: 'vue-svg-loader',
+                    options: {
+                        svgo: false
+                    }
+                }
+            ]
+            const imageMinPlugin = new ImageminPlugin({
+                pngquant: {
+                    quality: '5-30',
+                    speed: 7,
+                    strip: true
+                },
+                jpegtran: {
+                    progressive: true
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+                },
+                gifsicle: {
+                    interlaced: true
+                },
+                plugins: [
+                    imageminMozjpeg({
+                        quality: 70,
+                        progressive: true
+                    })
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+                ]
+            })
+            if (!ctx.isDev) config.plugins.push(imageMinPlugin)
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
+            config.module.rules.forEach(rule => {
+                if (rule.test.toString() === ORIGINAL_TEST) {
+                    rule.test = /\.(png|jpe?g|gif|webp)$/i
+                    rule.use = [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 1000,
+                                name: ctx.isDev ? '[path][name].[ext]' : 'img/[contenthash:7].[ext]'
+                            }
+                        }
+                    ]
+                }
+            })
+            //  Create the custom SVG rule
+            const svgRule = {
+                test: /\.svg$/,
+                oneOf: [
+                    {
+                        resourceQuery: /inline/,
+                        use: vueSvgLoader
+                    },
+                    {
+                        resourceQuery: /data/,
+                        loader: 'url-loader'
+                    },
+                    {
+                        resourceQuery: /raw/,
+                        loader: 'raw-loader'
+                    },
+                    {
+                        loader: 'file-loader' // By default, always use file-loader
+                    }
+                ]
+            }
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+            config.module.rules.push(svgRule) // Actually add the rule
+        }
+    }
 }
